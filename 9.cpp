52c381a76dbdd3da9a12c6b6d836de2334450702
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-09-18 18:58:48 +0530

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter two numbers";
+    cin>>a>>b;
+    if(a>=b)
+    {
+        cout<<"greater no. is "<<a;
+    }
+    else
+    {
+        cout<<"greater no. is "<<b;
+    }
+}
