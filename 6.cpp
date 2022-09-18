d6a8dd40e712770f3b5b7e21bc1f354918a226fb
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-09-18 18:58:30 +0530

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    double d;
+    cin>>a>>b>>c;
+    d=(a+b+c)/10.0;
+    cout<<"average of a three number is  "<<d;
+    
+}
