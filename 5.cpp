f912a53b2f1ad30f3bb692a3d6621c8282108218
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-09-18 18:58:19 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter a length , breadth and height of a cuboid ";
+    cin>>a>>b>>c;
+    cout<<endl<<"volume of a cuboid is "<<a*b*c<<" cubic unit";
+
+}
